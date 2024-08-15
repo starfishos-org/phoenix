6f01cae7c29d413d8d17f1df408ee66d74c889f4
yan <yanjingsheng@sjtu.edu.cn>
2024-08-15 09:31:13 +0800

    debug on matrix cxl

--- tests/basic/basic.c
+++ tests/basic/basic.c
@@ -4,11 +4,12 @@
 
 int main(int argc, char **argv) {
   int flags = atoi(argv[1]);
-  printf("flags %d\n", flags);
-  void *vm = mixed_malloc(0x100000, flags);
-  int *m = (int *)vm;
-  printf("pos:%p vm:%p\n", m, vm);
+  int size = atoi(argv[2]);
+  printf("flags %d size %d\n", flags, size);
+  int *m = (int *)mixed_malloc(0x100000, flags);
+  printf("pos:%p\n", m);
   m[0] = 1;
   printf("%d\n", m[0]);
+  free(m);
   return 0;
 }
