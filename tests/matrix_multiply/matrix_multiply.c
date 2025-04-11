d92e08906cb6207d26eb576ebf6dd104710e1264
Jingsheng Yan <yanjingsheng@sjtu.edu.cn>
2025-04-11 12:37:55 +0800

    Implement CPU binding functionality and enhance CMake configuration for CHCORE
    - Added a new function to parse CPU binding from a file and store the values in a global list.
    - Updated the thread pool implementation to utilize the CPU binding list if set.
    - Introduced a custom target in CMake to copy necessary data files for CHCORE OS.
    - Added new data files for matrix multiplication binding configurations.

--- tests/matrix_multiply/matrix_multiply.c
+++ tests/matrix_multiply/matrix_multiply.c
@@ -41,7 +41,7 @@
 
 #include "map_reduce.h"
 #include "stddefines.h"
-
+#include "processor.h"
 typedef struct {
     int row_num;
     int *matrix_A;
@@ -78,6 +78,8 @@ int matrix_len = 0;
 int row_block_len = 0;
 int file_size = 0;
 extern int thread_num;
+extern char thread_bind_cpu_filename[1024];
+extern bool thread_bind_cpu_set;
 
 void parse_args(int argc, char **argv) 
 {
@@ -89,7 +91,7 @@ void parse_args(int argc, char **argv)
     fname_A = "matrix_file_A.txt";
     fname_B = "matrix_file_B.txt";
 
-    while ((c = getopt(argc, argv, "l:r:t:m:c:")) != EOF) 
+    while ((c = getopt(argc, argv, "l:r:t:m:c:i:")) != EOF) 
     {
         switch (c) {
             case 'l':
@@ -109,8 +111,11 @@ void parse_args(int argc, char **argv)
             case 'c':
                 create_files = atoi(optarg);
                 break;
+            case 'i':
+                strcpy(thread_bind_cpu_filename, optarg);
+                break;
             case '?':
-                fprintf(stderr, "Usage: %s -l <side of matrix> -r <size of Row block> -t <thread_num> -m <0: default, 1: private, 2: shared> -c <create files>\n", argv[0]);
+                fprintf(stderr, "Usage: %s -l <side of matrix> -r <size of Row block> -t <thread_num> -i <thread bind cpu filename> -m <0: default, 1: private, 2: shared> -c <create files>\n", argv[0]);
                 exit(1);
         }
     }
@@ -129,6 +134,16 @@ void parse_args(int argc, char **argv)
     fprintf(stderr, "Memory malloc type=%d\n", memory_malloc_type);
     #endif
     fprintf(stderr, "Number of threads=%d\n", thread_num);
+    if (strlen(thread_bind_cpu_filename) == 0) {
+        fprintf(stderr, "Thread bind cpu filename is not set default to matrix_multiply_bind_cpu.txt\n");
+        strcpy(thread_bind_cpu_filename, "matrix_multiply_bind_cpu.txt");
+    }
+    fprintf(stderr, "Thread bind cpu filename=%s\n", thread_bind_cpu_filename);
+    if (parse_cpu_bind_file(thread_bind_cpu_filename) < 0) {
+        fprintf(stderr, "Failed to parse cpu bind file\n");
+    } else {
+        thread_bind_cpu_set = true;
+    }
 }
 
 /** myintcmp()
